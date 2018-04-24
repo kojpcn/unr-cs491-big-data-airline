45fb98ba0a3e179534b794e7bc133e090451cb78
Iridac <BLee@nevada.unr.edu>
2018-04-23 21:16:55 -0700

    Airline search by country

--- WordCount.java
+++ WordCount.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -12,51 +13,89 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class WordCount{
+public class WordCount {
 
-	public static class TokenizerMapper extends Mapper<IntWritable, Text, Text, String>{
+  public static class TokenizerMapper
+       extends Mapper<Object, Text, Text, NullWritable>{
 
-		private final static IntWritable one = new IntWritable(1);
-		private Text word = new Text();
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+    private String argument = "\"United States\"";
 
-		public void map(LongWritable key, Text value, Context context
-						) throws IOException, InterruptedException{
-			String[] split = value.toString().split(",+");
-			if (split.length > 4){
-				word.set(split[5]);
-				try{
-					context.write(word, split[0]);
-				} catch (NumberFormatException e){
-					// Cannot parse - ignore
-				}
-			}
-		}
-	}
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      String[] split = value.toString().split(",+");
+      if (split[3].equals(argument)) {
+        word.set(split[1]);
+        context.write(word, NullWritable.get());
+      }
+    }
+  }
 
-	public static class IntSumReducer extends Reducer<Text,String,Text,String>{
-		private String result = new String();
+  public static class IntSumReducer
+       extends Reducer<Text,NullWritable,Text,NullWritable> {
+    //private IntWritable result = new IntWritable();
 
-		public void reduce(Text key, Iterable<String> values, Context context) throws IOException, InterruptedException{
-			// int sum = 0;
-			for (String val : values){
-				// sum += val.get();
-				result = val;
-			}
-			context.write(key, result);
-		}
-	}
+    public void reduce(Text key, Iterable<NullWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      // int sum = 0;
+      // for (IntWritable val : values) {
+      //   sum += val.get();
+      // }
+      // result.set(sum);
+      context.write(key, NullWritable.get());
+    }
+  }
 
-	public static void main(String[] args) throws Exception{
-		Configuration conf = new Configuration();
-		Job job = Job.getInstance(conf, "word count");
-		job.setJarByClass(WordCount.class);
-		job.setMapperClass(TokenizerMapper.class);
-		job.setCombinerClass(IntSumReducer.class);
-		job.setReducerClass(IntSumReducer.class);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
-	}
+  public static class AirMapper
+       extends Mapper<Object, Text, Text, NullWritable>{
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+    private String argument = "\"United States\"";
+
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      String[] split = value.toString().split(",+");
+      if (split[3].equals(argument)) {
+        word.set(split[1]);
+        context.write(word, NullWritable.get());
+      }
+    }
+  }
+
+  public static class AirSumReducer
+       extends Reducer<Text,NullWritable,Text,NullWritable> {
+    //private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<NullWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      // int sum = 0;
+      // for (IntWritable val : values) {
+      //   sum += val.get();
+      // }
+      // result.set(sum);
+      context.write(key, NullWritable.get());
+    }
+  }
+
+  public static void test(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "word count");
+    job.setJarByClass(WordCount.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(NullWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+
+  public static void main(String[] args) throws Exception {
+  	test(args);
+  }
 }
