a31942398d71d197abb31f187b6b0b884166fdf9
kojpcn-VM <kojpcn@gmail.com>
2018-04-25 13:51:03 -0700

    Silenced hadoop output

--- run.sh
+++ run.sh
@@ -3,4 +3,6 @@ rm -rf output
 hadoop com.sun.tools.javac.Main WordCount.java
 jar cf wc.jar WordCount*.class
 
+export HADOOP_ROOT_LOGGER="WARN"
+
 hadoop jar wc.jar WordCount airports/ airlines/ routes/ output/
